9b37699db0bbf05337617782ce55f0349fd405f2
Prora6 <bubnovnikita1995@mail.ru>
2021-05-02 00:02:10 +0300

    дескрипшен готов адаптив

--- new/src/components/description/index.jsx
+++ new/src/components/description/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import'./style.scss'
 import Clip from'../../images/clip.png'
 import Notebook from '../../images/notebook.png'
+import Turned from'../../images/turned.png'
 
 class Description extends React.Component {
     render() {
@@ -12,8 +13,11 @@ class Description extends React.Component {
                         Собрать удалённое совещание на много человек. Организовать работу сотрудников вне офиса. Учесть новые правила безопасности. На плечи руководителей в этом году свалилось много нетривиальных задач. И они вряд ли исчезнут в 2021. Как справляться с этой нагрузкой? Мы решили узнать у издателя Лайфхакера, что входит в его топ-менеджерские задачи, и чем в их решении может помочь производительная техника.
                     </p>
                 </div>
-                <img src={Clip} alt="" className="description__img-clip"/>
-                <img src={Notebook} alt="" className="description__img-notebook"/>
+                <div className="description__img">
+                    <img src={Clip} alt="clip" className="description__img-clip"/>
+                    <img src={Notebook} alt="notebook" className="description__img-notebook"/>
+                    <img src={Turned} alt="turned" className="description__img-turned"/>
+                </div>
             </section>
         )
     }
