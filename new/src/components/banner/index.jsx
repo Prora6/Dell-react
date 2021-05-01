880b43c3effbc5ced7fa2e7577b2c5a45f5ef6dd
Prora6 <bubnovnikita1995@mail.ru>
2021-05-01 17:28:18 +0300

    баннер адаптив

--- new/src/components/banner/index.jsx
+++ new/src/components/banner/index.jsx
@@ -10,29 +10,54 @@ import Early from '../../images/early.png'
 import Cash from '../../images/Cash.png'
 import Work from '../../images/Work.png'
 import Thanks  from '../../images/thanks.png'
-
+import Reflection from '../../images/reflection.png'
+import Sandy from '../../images/Sandy.png'
+import Twitter from '../svgIcon/Twitter.jsx'
+import Vkontakte from '../svgIcon/Vkontakte.jsx'
+import Facebook from '../svgIcon/Facebook.jsx'
+import Money from '../../images/mini_money.png'
+import Mending from '../../images/mending.png'
+import Please from '../../images/please.png'
 
 class Banner extends React.Component {
     render() {
         return (
             <section className="banner">
                 <div className="banner__content">
-                    <div className="banner__content-title wrap">
-                        <h1 className="wrap__title">КАК УПРАВЛЯТЬ ЛЮДЬМИ</h1>
-                        <h3 className="wrap__description">и не сойти с ума в 2021</h3>             
+                    <div className="banner__content-information information">
+                        <h1 className="information__title">КАК УПРАВЛЯТЬ ЛЮДЬМИ</h1>
+                        <h3 className="information__description">и не сойти с ума в 2021</h3>             
                     </div>
-                    <div className="banner__content-img img">
-                        <img src={Man} alt="" className="img__man"/>
-                        <img src={Hi} alt="" className="img__hi"/>
-                        <img src={Early} alt="" className="img__early"/>
-                        <img src={Cash} alt="" className="img__cash"/>
-                        <img src={Work} alt="" className="img__work"/>
-                        <img src={Thanks} alt="" className="img__thanks"/>
+                    <div className="banner__content-social">
+                        <div className="link__wrap">
+                            <a href="" className="link">
+                                <Twitter  className="link__img"/>
+                            </a>
+                            <a href="" className="link">
+                                <Vkontakte className="link__img"/>
+                            </a>
+                            <a href="" className="link">
+                                <Facebook className="link__img"/>
+                            </a>
+                        </div>
                     </div>
-                    <img src={Folders} alt="Folders" className="banner__content-folders"/>
-                    <img src={Clock} alt="Clock" className="banner__content-clock"/>
-                    <img src={Cursor} alt="Cursor" className="banner__content-cursor"/>
-                    <img src={Picture} alt="Picture" className="banner__content-picture"/>
+                    <div className="banner__img img">
+                        <img src={Man} alt="" className="banner__img-man"/>
+                        <img src={Hi} alt="" className="banner__img-hi"/>
+                        <img src={Early} alt="" className="banner__img-early"/>
+                        <img src={Cash} alt="" className="banner__img-cash"/>
+                        <img src={Work} alt="" className="banner__img-work"/>
+                        <img src={Thanks} alt="" className="banner__img-thanks"/>
+                        <img src={Folders} alt="Folders" className="banner__img-folders"/>
+                        <img src={Clock} alt="Clock" className="banner__img-clock"/>
+                        <img src={Picture} alt="Picture" className="banner__img-picture"/>
+                        <img src={Cursor} alt="Cursor" className="banner__img-cursor"/>
+                        <img src={Reflection} alt="Reflection" className="banner__img-reflection"/>
+                        <img src={Sandy} alt="Sandy" className="banner__img-sandy"/>
+                        <img src={Money} alt="money" className="banner__img-money"/>
+                        <img src={Mending} alt="mending" className="banner__img-mending"/>
+                        <img src={Please} alt="please" className="banner__img-please"/>
+                    </div>
                 </div>
             </section>
         )
